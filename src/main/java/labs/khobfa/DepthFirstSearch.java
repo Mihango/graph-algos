9240e66292b8663f59c5a4c35b6f6b8c9c07f921
Ken <mihangokennedy@gmail.com>
2020-09-17 18:09:54 +0300

    added breath first search

--- src/main/java/labs/khobfa/DepthFirstSearch.java
+++ src/main/java/labs/khobfa/DepthFirstSearch.java
@@ -1,4 +1,27 @@
 package labs.khobfa;
 
+import java.util.Stack;
+
 public class DepthFirstSearch {
+
+    public Iterable<Integer> dfs(Graph graph, int source) {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[graph.vertices];
+        dfs(graph, source, visited, stack);
+        return stack;
+    }
+
+    private void dfs(Graph graph, int source, boolean[] visited, Stack<Integer> stack) {
+        if (visited[source]) return;
+
+        visited[source] = true;
+        stack.add(source);
+
+        for (int node : graph.getAdjacent(source)) {
+            if (!visited[node]) {
+                visited[node] = true;
+                dfs(graph, node, visited, stack);
+            }
+        }
+    }
 }
